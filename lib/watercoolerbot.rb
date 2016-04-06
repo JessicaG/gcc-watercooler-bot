2379327113dfaffa95dbd1266a87c94d0313753d
jessicag <jgoulding2@gmail.com>
2016-04-06 12:02:49 -0600

    testing weather response

--- lib/watercoolerbot.rb
+++ lib/watercoolerbot.rb
@@ -4,6 +4,10 @@ class WaterCoolerBot < SlackRubyBot::Bot
   command 'ping' do |client, data, match|
     client.say(text: 'pong', channel: data.channel)
   end
+
+  match /^How is the weather in (?<location>\w*)\?$/ do |client, data, match|
+    client.say(channel: data.channel, text: "The weather in #{match[:location]} is nice.")
+  end
 end
 
 WaterCoolerBot.run
